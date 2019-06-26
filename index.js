c42e03f25d12486df4f3f941212a8e09dad10b6e
Hema Venkata <hvenkata@live.com>
2019-06-26 01:14:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function iterativeLog(array) {
-  array.forEach();
+  array.forEach((index, element) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var newArray = ["call", "me", "mother"]
+  newArray.forEach(callback)
+  return newArray
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
 }
